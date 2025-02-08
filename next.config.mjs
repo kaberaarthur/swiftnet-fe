f2ec8492469ac40bae130f90a5072fec2155a1f3
Arthur Kabera <arthurkabera@gmail.com>
2025-02-08 08:22:22 +0300

    Working

--- next.config.mjs
+++ next.config.mjs
@@ -23,7 +23,7 @@ const nextConfig = {
       {
         source: "/microservice/:path*", // Matches API paths
         // destination: "http://localhost:8000/:path*", // Embed username:password in the URL
-        destination: "http://localhost:3001/:path*", // Embed username:password in the URL
+        destination: "http://139.59.60.20:3001/:path*", // Embed username:password in the URL
       },
       {
         source: "/test",
