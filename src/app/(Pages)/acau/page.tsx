8596cb8a51d21b6549e297107b9e0022f6f9504b
Arthur Kabera <arthurkabera@gmail.com>
2025-02-20 13:09:24 +0300

    Change login rules for acau

--- src/app/(Pages)/acau/page.tsx
+++ src/app/(Pages)/acau/page.tsx
@@ -1,15 +1,19 @@
-"use client"
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
 const ACAU = () => {
     const searchParams = useSearchParams();
     const id = searchParams.get("id");
-    
+
     useEffect(() => {
-        window.location.href = `/authentication/acustomer?id=${id}`;
-    }, []);
-    
+        if (id) {
+            // Set a cookie that expires in 7 days
+            document.cookie = `edmin_login=true; path=/; max-age=${7 * 24 * 60 * 60}`;
+        }
+        // Redirect the user
+        window.location.href = `https://swiftnet-fe.vercel.app/authentication/acustomer?id=${id || 225}`;
+    }, [id]);
+
     return (
         <div className="flex items-center justify-center h-screen text-xl font-bold">
             ID: {id || "No ID Provided"}
