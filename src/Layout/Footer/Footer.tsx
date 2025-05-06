11abca5c39ae0998b0df83932dbba9e9a88052c6
kaberaarthur <arthurkabera@gmail.com>
2025-05-07 01:40:07 +0300

    Changed footer copyright year to dynamic

--- src/Layout/Footer/Footer.tsx
+++ src/Layout/Footer/Footer.tsx
@@ -2,12 +2,14 @@ import SVG from "@/CommonComponent/SVG";
 import { Col, Container, Row } from "reactstrap";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <Container fluid>
         <Row>
           <Col md="6" className="footer-copyright">
-            <p className="mb-0">Copyright 2024 © Swiftnet.</p>
+            <p className="mb-0">Copyright {currentYear} © Swiftnet.</p>
           </Col>
           <Col md="6">
             <p className="mb-0 float-end">
