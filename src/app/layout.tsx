39821c1e67e1b125627405a7a27322519f5a9b6f
kaberaarthur <arthurkabera@gmail.com>
2025-08-27 13:54:57 +0300

    Added Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { detectLanguage } from "./i18n/server";
 
 export const metadata: Metadata = {
   title: "Swiftnet - ISP Hub",
-  description: "Created by Swiftnet Internet",
+  description: "An Intiuitive ISP Management System",
 };
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 
@@ -23,6 +23,15 @@ const lng = await detectLanguage();
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAjeJEPREBQFvAIqDSZliF0WjQrCld-Mh0"></script>
+        <script type="text/javascript">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t1chsnyq8n");
+          `}
+        </script>
       </head>
       <body>
         <NoSsr>
